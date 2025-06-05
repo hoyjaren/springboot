f428508e2d8716f71f1ea058d704fe2f6682fbb9
hoyjaren <jarentabutabo12345@gmail.com>
2025-06-06 00:06:50 +0800

    final commit

--- store/store/src/main/java/com/codewithjaren/store/OrderService.java
+++ store/store/src/main/java/com/codewithjaren/store/OrderService.java
@@ -1,11 +1,19 @@
 package com.codewithjaren.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
-//    public OrderService(PaymentService paymentService){
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService, int x){}
+
+    @Autowired
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder(){
         paymentService.processPayment(20);
